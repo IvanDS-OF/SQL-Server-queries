ad5a4e4c470a63067cf9f7f5535a7be6ef6cf866
IvanDS-OF <ivan.durans@outlook.com>
2024-07-14 18:37:26 -0600

    File modified, adding new queties

--- Primeras Queries.sql
+++ Primeras Queries.sql
@@ -17,3 +17,15 @@ SELECT [SubTotal], [TaxAmt], [Freight], [TotalDue],
 	WHERE SalesOrderID='43661';
 
 
+SELECT TOP 10 *
+	FROM [Sales].[SalesOrderHeader]
+	---WHERE TotalDue BETWEEN 3 AND 5
+	WHERE TotalDue>=4 AND TotalDue<6
+	ORDER BY  TotalDue
+
+
+
+
+
+
+
