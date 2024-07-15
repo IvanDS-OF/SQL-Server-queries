374f5561a378ba21186d80b58b0fbc24370ac4d2
IvanDS-OF <ivan.durans@outlook.com>
2024-07-15 10:44:05 -0600

    Knowing how to show specific information form indexes

--- New Sentences.sql
+++ New Sentences.sql
@@ -1,1 +1,31 @@
 --Vamos a escribir nuevas sentencias relacionadas más a T SQL
+
+--Creamos nuestras consultas bases para comenzar a modificarlas y obtener valores especificos
+
+SELECT TOP 20 *
+FROM [Production].[Product]
+ORDER BY [ListPrice] DESC
+
+--Así nosotros podemos hacer una ocnsulta de valores determinado un index inicial más
+--un numero de valores abajo de ese indice inicial. Vamos a obtener 10 registros a partir de
+--la fila 4
+SELECT *
+FROM [Production].[Product]
+ORDER BY [ListPrice] DESC
+OFFSET 4 ROWS FETCH NEXT 10 ROWS ONLY
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
